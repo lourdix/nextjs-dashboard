62ce3017e2dd0623dbb207a9b43f0fd73370e341
Lourdix <mouss.du.95@gmail.com>
2025-02-02 12:26:38 +0100

    Disabling linting during production builds

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Mouss Dashboard',
+    default: 'Mouss Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
